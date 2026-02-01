e7b071f69d87391bc1ad58854ea96a2cb133db69
Neevash Ramdial (Nash) <mail@neevash.dev>
2026-02-01 15:17:38 -0800

    Initial Commit

---
+++ rayban_agents/rayban_agentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  rayban_agentsApp.swift
+//  rayban_agents
+//
+//  Created by Neevash Ramdial on 2/1/26.
+//
+
+import SwiftUI
+
+@main
+struct rayban_agentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ rayban_agentsTests/rayban_agentsTests.swift
@@ -0,0 +1,17 @@
+//
+//  rayban_agentsTests.swift
+//  rayban_agentsTests
+//
+//  Created by Neevash Ramdial on 2/1/26.
+//
+
+import Testing
+@testable import rayban_agents
+
+struct rayban_agentsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
